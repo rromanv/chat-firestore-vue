230210519428bfd489f21a8d60f42ef6edb254b9
Rodolfo Roman <rromanv@gmail.com>
2020-04-02 23:13:26 -0500

    Added Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -10,12 +12,21 @@ const routes = [
     name: 'Home',
     component: Home,
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  to.name === 'Login' || store.getters.getUser ? next() : next('/login')
 })
 
 export default router
