e6d076dd5afb3d284a9360bcfd225c8cbfbfb1a3
Rodolfo Roman <rromanv@gmail.com>
2020-04-02 23:17:05 -0500

    Added NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <NavBar />
     <v-content>
       <router-view />
     </v-content>
@@ -7,7 +8,11 @@
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
 export default {
   name: 'App',
+  components: {
+    NavBar,
+  },
 }
 </script>
